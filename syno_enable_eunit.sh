46e9df236737c4653fc51f072e5feb25f4a8dd29
Dave Russell <39733752+007revad@users.noreply.github.com>
2025-09-19 06:43:32 +1000

    Update disable_ncq function for multiple same eunits

--- syno_enable_eunit.sh
+++ syno_enable_eunit.sh
@@ -12,7 +12,7 @@
 # sudo -i /volume1/scripts/syno_enable_eunit.sh
 #-----------------------------------------------------------------------------------
 
-scriptver="v3.0.20"
+scriptver="v3.0.21"
 script=Synology_enable_eunit
 repo="007revad/Synology_enable_eunit"
 scriptname=syno_enable_eunit
@@ -1025,10 +1025,10 @@ edit_modeldtb(){
 }
 
 disable_ncq(){ 
-    # Disable NCQ for drives in the DX (set queue depth of NCQ to 1 element)
+    # Disable NCQ for drives in the DX (set queue depth of NCQ to 1 element) - credit @steinreiter
     DX_NAME="$1"      # Capitalization of the name is important
     DX_DISK_NUM="$2"  # Max number of disks supported by the expansion unit
-    DX_DISKS=$(syno_disk_dump | grep "^Eunit disk info: $DX_NAME\$" -A "$DX_DISK_NUM" | tail -n+2 | head -n"$DX_DISK_NUM" | cut -d: -f2 | cut -f1 | xargs)
+	DX_DISKS=$(syno_disk_dump | grep "^Eunit disk info: $DX_NAME\$" -A "$DX_DISK_NUM" | cut -d: -f2 | cut -f1 | xargs -L1 | grep -v "^$DX_NAME\$")
     for dx_disk in $DX_DISKS; do
         syno_disk_ctl --ncq-off "$dx_disk"
     done
